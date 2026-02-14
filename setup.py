d0e44a68bf4121f529d1bc06b592d8249a8ad9bb
Aniket <iamaniketkumarmaner@gmail.com>
2026-02-14 19:59:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="minichain",
     version="0.1.0",
-    packages=find_packages(),  # Will detect core, consensus, network, etc.
+    packages=find_packages(),
     py_modules=["main"],
     install_requires=[
-        "pynacl",
-        "py-libp2p",
+        "pynacl==1.6.2",
+        "libp2p==0.5.0",  # Fixed: was "py-libp2p"
     ],
     entry_points={
         "console_scripts": [
-            "minichain=main:main",  # Points to main.py -> main()
+            "minichain=main:main",  # Requires main() function in main.py
         ],
     },
     python_requires=">=3.8",
